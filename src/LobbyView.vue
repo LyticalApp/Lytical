965755ee41d6629fc46d9d1c74b1b82060cdc8fa
downthecrop <bryce@mathx.ca>
2022-10-13 09:52:41 +0900

    Resolve issue of using champion display names instead of internal names

--- src/LobbyView.vue
+++ src/LobbyView.vue
@@ -1,78 +1,74 @@
 <template>
-    <div class="wrapper">
-        <LCUErrorMessage v-if="showError" />
-        <div v-if="showTimeout">
-          <h1>Unable to connect to lobby</h1>
-        </div>
-        <div class="team">
-            <div v-for="teammate in lobbyPlayers" :key="teammate.displayName">
-                <div v-if="teammate.teamId == undefined || teammate.teamId == 1" class="miniCard">
-                    <img v-if="teammate.championId"
-                    class="selectedChampion emblem" :src="CHAMPIONICONURL+teammate.championId+'.png'">
-                    <img class="emblem" :src='`assets/Emblem_${teammate.queueMap.RANKED_SOLO_5x5.tier}.webp`'>
-                    <br>
-                    <span @click="searchSummoner(teammate.username)">{{teammate.username}}</span>
-                    <br>
-                    <span style="font-size:12px;">{{capitalize(teammate.queueMap.RANKED_SOLO_5x5.tier)}}
-                        {{romanNumbers[teammate.queueMap.RANKED_SOLO_5x5.division]}}
-                        ({{teammate.queueMap.RANKED_SOLO_5x5.leaguePoints}}LP)</span>
-                    <br>
-                    <span style="font-size:12px;">{{capitalize(teammate.position)}}</span>
-                    <br>
-                    <div v-for="(match,index) in teammate.matchHistory.games.games" :key="match.gameId"
-                        class="matchItem">
-                        <div v-if="!(teammate.teamId == 1 && index > 4)">
-                            <img @click="open(getPreferredSite(match.participants[0].championId))"
-                                :src="CHAMPIONICONURL+match.participants[0].championId+'.png'"
-                                class="championIcon">
-                            <div :class='`kda ${match.participants[0].stats.win ? "Victory" : "Defeat"}`'>
-                                <span style="white-space:nowrap">
-                                  {{match.participants[0].stats.kills + "/"
-                                  + match.participants[0].stats.deaths + "/"
-                                  + match.participants[0].stats.assists}}
-                                </span>
-                            </div>
-                            <span class="sinceGame">{{sinceGame(Date.now()-match.gameCreation)}}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="team">
-            <div v-for="teammate in lobbyPlayers" :key="teammate.displayName">
-                <div v-if="teammate.teamId == 2" class="miniCard">
-                  <img v-if="teammate.championId"
-                    class="selectedChampion emblem" :src="CHAMPIONICONURL+teammate.championId+'.png'">
-                    <img class="emblem" :src='`assets/Emblem_${teammate.queueMap.RANKED_SOLO_5x5.tier}.webp`'>
-                    <br>
-                    <span @click="searchSummoner(teammate.username)">{{teammate.username}}</span>
-                    <br>
-                    <span style="font-size:12px;">{{capitalize(teammate.queueMap.RANKED_SOLO_5x5.tier)}}
-                        {{romanNumbers[teammate.queueMap.RANKED_SOLO_5x5.division]}}
-                        ({{teammate.queueMap.RANKED_SOLO_5x5.leaguePoints}}LP)</span>
-                    <br>
-                    <span style="font-size:12px;">{{capitalize(teammate.position)}}</span>
-                    <br>
-                    <div v-for="(match,index) in teammate.matchHistory.games.games" :key="match.gameId"
-                        class="matchItem">
-                        <div v-if="index < 5">
-                            <img @click="open(getPreferredSite(match.participants[0].championId))"
-                                :src="CHAMPIONICONURL+match.participants[0].championId+'.png'"
-                                class="championIcon">
-                            <div :class='`kda ${match.participants[0].stats.win ? "Victory" : "Defeat"}`'>
-                                <span style="white-space:nowrap">
-                                  {{match.participants[0].stats.kills + "/"
-                                  + match.participants[0].stats.deaths + "/"
-                                  + match.participants[0].stats.assists}}
-                                </span>
-                            </div>
-                            <span class="sinceGame">{{sinceGame(Date.now()-match.gameCreation)}}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="wrapper">
+    <LCUErrorMessage v-if="showError" />
+    <div v-if="showTimeout">
+      <h1>Unable to connect to lobby</h1>
     </div>
+    <div class="team">
+      <div v-for="teammate in lobbyPlayers" :key="teammate.displayName">
+        <div v-if="teammate.teamId == undefined || teammate.teamId == 1" class="miniCard">
+          <img v-if="teammate.championId" class="selectedChampion emblem"
+            :src="CHAMPIONICONURL+teammate.championId+'.png'">
+          <img class="emblem" :src='`assets/Emblem_${teammate.queueMap.RANKED_SOLO_5x5.tier}.webp`'>
+          <br>
+          <span @click="searchSummoner(teammate.username)">{{teammate.username}}</span>
+          <br>
+          <span style="font-size:12px;">{{capitalize(teammate.queueMap.RANKED_SOLO_5x5.tier)}}
+            {{romanNumbers[teammate.queueMap.RANKED_SOLO_5x5.division]}}
+            ({{teammate.queueMap.RANKED_SOLO_5x5.leaguePoints}}LP)</span>
+          <br>
+          <span style="font-size:12px;">{{capitalize(teammate.position)}}</span>
+          <br>
+          <div v-for="(match,index) in teammate.matchHistory.games.games" :key="match.gameId" class="matchItem">
+            <div v-if="!(teammate.teamId == 1 && index > 4)">
+              <img @click="open(getPreferredSite(match.participants[0].championId))"
+                :src="CHAMPIONICONURL+match.participants[0].championId+'.png'" class="championIcon">
+              <div :class='`kda ${match.participants[0].stats.win ? "Victory" : "Defeat"}`'>
+                <span style="white-space:nowrap">
+                  {{match.participants[0].stats.kills + "/"
+                  + match.participants[0].stats.deaths + "/"
+                  + match.participants[0].stats.assists}}
+                </span>
+              </div>
+              <span class="sinceGame">{{sinceGame(Date.now()-match.gameCreation)}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="team">
+      <div v-for="teammate in lobbyPlayers" :key="teammate.displayName">
+        <div v-if="teammate.teamId == 2" class="miniCard">
+          <img v-if="teammate.championId" class="selectedChampion emblem"
+            :src="CHAMPIONICONURL+teammate.championId+'.png'">
+          <img class="emblem" :src='`assets/Emblem_${teammate.queueMap.RANKED_SOLO_5x5.tier}.webp`'>
+          <br>
+          <span @click="searchSummoner(teammate.username)">{{teammate.username}}</span>
+          <br>
+          <span style="font-size:12px;">{{capitalize(teammate.queueMap.RANKED_SOLO_5x5.tier)}}
+            {{romanNumbers[teammate.queueMap.RANKED_SOLO_5x5.division]}}
+            ({{teammate.queueMap.RANKED_SOLO_5x5.leaguePoints}}LP)</span>
+          <br>
+          <span style="font-size:12px;">{{capitalize(teammate.position)}}</span>
+          <br>
+          <div v-for="(match,index) in teammate.matchHistory.games.games" :key="match.gameId" class="matchItem">
+            <div v-if="index < 5">
+              <img @click="open(getPreferredSite(match.participants[0].championId))"
+                :src="CHAMPIONICONURL+match.participants[0].championId+'.png'" class="championIcon">
+              <div :class='`kda ${match.participants[0].stats.win ? "Victory" : "Defeat"}`'>
+                <span style="white-space:nowrap">
+                  {{match.participants[0].stats.kills + "/"
+                  + match.participants[0].stats.deaths + "/"
+                  + match.participants[0].stats.assists}}
+                </span>
+              </div>
+              <span class="sinceGame">{{sinceGame(Date.now()-match.gameCreation)}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -203,7 +199,7 @@ export default {
               id: 'lol-match-playercard',
               position: player.position,
               summonerName: player.summonerName,
-              championId: namesToId[player.championName.replace(/\s/g, '')],
+              championId: namesToId[player.rawChampionName.replace('game_character_displayname_', '')],
               teamId: (player.team === 'ORDER') ? 1 : 2,
             });
           }
@@ -239,72 +235,82 @@ export default {
 
 <style scoped>
 .championIcon {
-  border-radius:10px;
-  width:20px;
-  height:20px;
+  border-radius: 10px;
+  width: 20px;
+  height: 20px;
 }
+
 .kda {
-    margin-left:10px;
-    padding:0 10px 0px 10px;
-    font-size:15px;
-    color: var(--dark-grey);
-    text-align:center !important;
-    display:inline-block;
-    width:55px;
-    border-radius:4px;
-    font-weight: 400;
+  margin-left: 10px;
+  padding: 0 10px 0px 10px;
+  font-size: 15px;
+  color: var(--dark-grey);
+  text-align: center !important;
+  display: inline-block;
+  width: 55px;
+  border-radius: 4px;
+  font-weight: 400;
 }
+
 .emblem {
-  height:70px;
+  height: 70px;
 }
+
 .selectedChampion {
-  border-radius:50%;
+  border-radius: 50%;
 }
+
 .Victory {
-    color: var(--lobby-victory-text);
-    background-color: var(--lobby-victory-background);
+  color: var(--lobby-victory-text);
+  background-color: var(--lobby-victory-background);
 }
+
 .Defeat {
-    color: var(--lobby-defeat-text);
-    background-color: var(--lobby-defeat-background);
+  color: var(--lobby-defeat-text);
+  background-color: var(--lobby-defeat-background);
 }
+
 .sinceGame {
-  font-size:15px;
-  margin-left:4px;
-  width:28px;
-  display:inline-block;
+  font-size: 15px;
+  margin-left: 4px;
+  width: 28px;
+  display: inline-block;
   background-color: var(--minicard-since-background);
-  border-radius:4px;
-  padding-left:4px;
-  padding-right:4px;
+  border-radius: 4px;
+  padding-left: 4px;
+  padding-right: 4px;
   text-align: center;
 }
+
 .matchItem {
-    margin:auto;
-    width:150px;
-    text-align:left;
+  margin: auto;
+  width: 150px;
+  text-align: left;
 }
+
 .team {
-    display: flex;
-    max-width: 1130px;
+  display: flex;
+  max-width: 1130px;
 }
+
 .miniCard {
-    min-height: 232px;
-    margin: 10px;
-    padding:10px;
-    color: var(--minicard-text);
-    border-radius: 5px;
-    background-color: var(--minicard-background);
-    filter: drop-shadow(0 0 0.1rem #5cd7e4);
+  min-height: 232px;
+  margin: 10px;
+  padding: 10px;
+  color: var(--minicard-text);
+  border-radius: 5px;
+  background-color: var(--minicard-background);
+  filter: drop-shadow(0 0 0.1rem #5cd7e4);
 }
+
 .wrapper {
-    margin:auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    overflow-y:scroll;
-    height:calc(100vh - 42px);
-    flex-wrap: wrap;
-    justify-content: center;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll;
+  height: calc(100vh - 42px);
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
